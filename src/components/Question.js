ce9f8419d7f4b0cb6bdfa20e083aac342026b193
Josette Garcia <josettetgarcia@gmail.com>
2022-03-29 22:29:02 -0500

    commit tree d2045217aeeab938ebd896faef44c263543a1c17

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  // useEffect(()=>{
+  //   const setTimeout = setInterval(()=> { 
+  //     if(timeRemaining > 0){
+  //     setTimeRemaining(()=>(timeRemaining -1))
+  //   }
+  // }, 1000);
+
+  //   return function cleanup(){
+  //     clearInterval(setTimeout)
+  //   }
+  // },[])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1)
+    }, 1000)
+    return () => clearTimeout(timer)
+  }, [timeRemaining])
+
+  if (timeRemaining === 0) {
+    setTimeRemaining(10)
+    onAnswered(false);
+  }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -30,3 +53,4 @@ function Question({ question, onAnswered }) {
 }
 
 export default Question;
+ 
